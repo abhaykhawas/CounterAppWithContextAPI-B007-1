0ea6b4be63b440892bc40ffa18b73fa47faeec20
abhay khawas <abhaykhawas@gmail.com>
2025-08-01 23:08:18 +0530

    changed name again

--- src/ToggleThemeButton.jsx
+++ src/ToggleThemeButton.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import ThemeContext from './themeContext'
+import ThemeContext from './ThemeContext'
 
 function ToggleThemeButton() {
     const { theme, toggleTheme } = useContext(ThemeContext)
